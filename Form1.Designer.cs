738981788fcd58a616f63a85e9c49f7ffb23f63f
nihat23 <62906442+nihat23@users.noreply.github.com>
2021-03-21 16:24:28 +0300

    Faiz_Hesaplama
    faiz hesaplama ile ilgili

---
+++ Form1.Designer.cs
@@ -0,0 +1,332 @@
+﻿namespace Faiz_Hesaplama
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox5fiyat = new System.Windows.Forms.TextBox();
+            this.textBox5Kdv = new System.Windows.Forms.TextBox();
+            this.textBox5otv = new System.Windows.Forms.TextBox();
+            this.textBox5Toplam = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 35);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ana para :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 86);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(138, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Faiz oranı  :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(42, 151);
+            this.label3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Faiz yıl :  ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(393, 35);
+            this.label4.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(153, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Faiz kazanç :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(393, 96);
+            this.label5.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(157, 25);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Toplam para :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(157, 35);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(198, 31);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(157, 86);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(198, 31);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Enabled = false;
+            this.textBox3.Location = new System.Drawing.Point(554, 35);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(198, 31);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Enabled = false;
+            this.textBox4.Location = new System.Drawing.Point(558, 93);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(198, 31);
+            this.textBox4.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(247, 207);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 52);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Hesapla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(457, 207);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(180, 52);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Temizle";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(157, 151);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(129, 33);
+            this.comboBox1.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(20, 283);
+            this.label6.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(78, 25);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Fiyat :";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(20, 328);
+            this.label8.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(66, 25);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Kdv :";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(20, 370);
+            this.label9.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(62, 25);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "Ötv :";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(-5, 415);
+            this.label10.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(103, 25);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "Toplam :";
+            // 
+            // textBox5fiyat
+            // 
+            this.textBox5fiyat.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox5fiyat.Location = new System.Drawing.Point(116, 280);
+            this.textBox5fiyat.Name = "textBox5fiyat";
+            this.textBox5fiyat.Size = new System.Drawing.Size(198, 26);
+            this.textBox5fiyat.TabIndex = 18;
+            // 
+            // textBox5Kdv
+            // 
+            this.textBox5Kdv.Enabled = false;
+            this.textBox5Kdv.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox5Kdv.Location = new System.Drawing.Point(116, 329);
+            this.textBox5Kdv.Name = "textBox5Kdv";
+            this.textBox5Kdv.Size = new System.Drawing.Size(198, 26);
+            this.textBox5Kdv.TabIndex = 19;
+            // 
+            // textBox5otv
+            // 
+            this.textBox5otv.Enabled = false;
+            this.textBox5otv.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox5otv.Location = new System.Drawing.Point(116, 369);
+            this.textBox5otv.Name = "textBox5otv";
+            this.textBox5otv.Size = new System.Drawing.Size(198, 26);
+            this.textBox5otv.TabIndex = 20;
+            // 
+            // textBox5Toplam
+            // 
+            this.textBox5Toplam.Enabled = false;
+            this.textBox5Toplam.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox5Toplam.Location = new System.Drawing.Point(116, 416);
+            this.textBox5Toplam.Name = "textBox5Toplam";
+            this.textBox5Toplam.Size = new System.Drawing.Size(198, 26);
+            this.textBox5Toplam.TabIndex = 21;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Black;
+            this.button3.Location = new System.Drawing.Point(346, 304);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(114, 37);
+            this.button3.TabIndex = 22;
+            this.button3.Text = "Hesapla";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Black;
+            this.button4.Location = new System.Drawing.Point(346, 362);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(114, 37);
+            this.button4.TabIndex = 23;
+            this.button4.Text = "Temizle";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Chocolate;
+            this.ClientSize = new System.Drawing.Size(797, 472);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox5Toplam);
+            this.Controls.Add(this.textBox5otv);
+            this.Controls.Add(this.textBox5Kdv);
+            this.Controls.Add(this.textBox5fiyat);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Faiz Hesaplama ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBox5fiyat;
+        private System.Windows.Forms.TextBox textBox5Kdv;
+        private System.Windows.Forms.TextBox textBox5otv;
+        private System.Windows.Forms.TextBox textBox5Toplam;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Faiz_Hesaplama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void temizle()
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            comboBox1.ResetText();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for(int i=1; i < 51; i++)
+            {
+                comboBox1.Items.Add(i);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            temizle();
+        }
+
+
+        double faiz, anapara, faizorani, faizyili, toplamkazanc;
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox5fiyat.Clear();
+            textBox5Kdv.Text = "";
+            textBox5otv.Clear();
+            textBox5Toplam.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            const double Kdv_orani=0.18, Otv_orani = 0.45;
+            double fiyat, kdv_tutari = 0, Otv_tutari = 0, Toplam = 0;
+
+            fiyat =double.Parse(textBox5fiyat.Text);
+            kdv_tutari = fiyat * Kdv_orani;
+            Otv_tutari = fiyat * Otv_orani;
+            
+            Toplam = fiyat + kdv_tutari + Otv_tutari;
+
+            textBox5Kdv.Text = kdv_tutari.ToString()+ " TL";
+            textBox5otv.Text = Otv_tutari.ToString()+ " TL";
+            textBox5Toplam.Text = Toplam.ToString()+ " TL";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                anapara = double.Parse(textBox1.Text);
+                faizorani = double.Parse(textBox2.Text);
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Tüm alanları doldurunuz", "UYARI", MessageBoxButtons.OK,MessageBoxIcon.Information);
+            }
+            
+            faizyili =Convert.ToDouble(comboBox1.SelectedItem);
+
+            faiz = anapara * faizorani * faizyili / 100;
+            textBox3.Text = faiz.ToString();
+
+            toplamkazanc = faiz + anapara;
+            textBox4.Text = toplamkazanc.ToString();
+
+
+        }
+    }
+}
